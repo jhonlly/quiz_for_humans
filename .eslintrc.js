160b61b17bb5b6034ae9f7ba2c29bc10d30e5f40
Jhon Rodriguez <jhon.rodriguez@altia.es>
2022-05-21 12:19:50 +0100

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'eslint:recommended'
+  ],
+  globals:{
+    window: true,
+    module: true,
+  },
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 2020,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    'no-console': 'warn',
+    'react/prop-types': 0
+  }
+};
